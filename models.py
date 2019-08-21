baa7dc2ecf35c65a4ff8b3f58005f1f9a2742208
“ChoJaeSeong”  <tingstyle1@gmail.com>
2019-08-21 17:31:12 +0900

    [2019-08-21] models.py 수정 + location까지 db등록 - models.py 수정(class Similaritygroup -> SimilarityGroup) : 테이블매핑은 similarity_group - similarity_group, category -> herb -> location db insert( 파이참에서 입력, commit버튼 필수!)

--- models.py
+++ models.py
@@ -1,7 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
 
-
 db = SQLAlchemy()
+
+
+# 4-4. 유사약재그룹
+class SimilarityGroup(db.Model):
+    # PK, FK
+    group_id = db.Column(db.Integer, primary_key=True)
+    # 일반속성
+    group_name = db.Column(db.String(255))
+    # 각 테이블에 대한 관계설정
+    herb = db.relationship('Herb', backref="group_id")
+
+    def __repr__(self):
+        return '<SimilarityGroup %r>' % (self.group_name)
+
+
+# 4-5. 한방분류그룹
+class Category(db.Model):
+    # PK, FK
+    category_id = db.Column(db.Integer, primary_key=True)
+    # 일반속성
+    category_name = db.Column(db.String(255))
+    # 각 테이블에 대한 관계설정
+    herb = db.relationship('Herb', backref="category_id")
+
+    def __repr__(self):
+        return '<Category %r>' % (self.category_name)
 
 
 class Herb(db.Model):
@@ -9,8 +34,8 @@ class Herb(db.Model):
         db.CheckConstraint('is_poison = 0 or is_poison = 1'),
     )
     # PK, FK
-    herb_id = db.Column(db.Integer, primary_key=True ) # PK면서, Location의 FK -> Location에서 선택당해야함
-    group_id_fk = db.Column(db.Integer, db.ForeignKey('similaritygroup.group_id'))
+    herb_id = db.Column(db.Integer, primary_key=True)  # PK면서, Location의 FK -> Location에서 선택당해야함
+    group_id_fk = db.Column(db.Integer, db.ForeignKey('similarity_group.group_id'))
     category_id_fk = db.Column(db.Integer, db.ForeignKey('category.category_id'))
 
     # 일반속성
@@ -19,7 +44,7 @@ class Herb(db.Model):
     effect = db.Column(db.String(255))
     origin = db.Column(db.String(255))
     scientific_name = db.Column(db.String(255))
-    is_poison = db.Column(db.Integer, db.CheckConstraint('is_poison = 0 or is_poison = 1')) # 확인필요
+    is_poison = db.Column(db.Integer, db.CheckConstraint('is_poison = 0 or is_poison = 1'))  # 확인필요
     figure_feature = db.Column(db.Text, nullable=True)
     image_path = db.Column(db.Text, nullable=True)
     # 각 테이블에 대한 관계설정 (admin에서 나와버리니까 안나오도록)
@@ -30,49 +55,27 @@ class Herb(db.Model):
     def __repr__(self):
         return '<Herb %r>' % (self.herb_name)
 
-# 4-4. 유사약재그룹
-class Similaritygroup(db.Model):
-    # PK, FK
-    group_id = db.Column(db.Integer, primary_key=True )
-    # 일반속성
-    group_name = db.Column(db.String(255))
-    # 각 테이블에 대한 관계설정
-    herb = db.relationship('Herb', backref="group_id")
-
-    def __repr__(self):
-        return '<Similaritygroup %r>' % (self.group_name)
-
-# 4-5. 한방분류그룹
-class Category(db.Model):
-    # PK, FK
-    category_id = db.Column(db.Integer, primary_key=True )
-    # 일반속성
-    category_name = db.Column(db.String(255))
-    # 각 테이블에 대한 관계설정
-    herb = db.relationship('Herb', backref="category_id")
-
-    def __repr__(self):
-        return '<Category %r>' % (self.category_name)
 
 # 4-3. Location
 class Location(db.Model):
     # PK, FK
-    location_id = db.Column(db.Integer, primary_key=True )
-    herb_id_fk = db.Column(db.Integer, db.ForeignKey('herb.herb_id')) # FK인데, 자기쪽에선 PK
+    location_id = db.Column(db.Integer, primary_key=True)
+    herb_id_fk = db.Column(db.Integer, db.ForeignKey('herb.herb_id'))  # FK인데, 자기쪽에선 PK
 
     # 일반속성
     country = db.Column(db.String(150))
-    location_name = db.Column(db.String(150), nullable=True) # 지역명! 지역명없을땐 국가는 외국
+    location_name = db.Column(db.String(150), nullable=True)  # 지역명! 지역명없을땐 국가는 외국
     pos_x = db.Column(db.Float)
     pos_y = db.Column(db.Float)
 
     def __repr__(self):
         return '<Location %r>' % (self.location_name)
 
+
 # 4-7. 뉴스
 class News(db.Model):
     # PK, FK
-    news_id = db.Column(db.Integer, primary_key=True )
+    news_id = db.Column(db.Integer, primary_key=True)
     herb_id_fk = db.Column(db.Integer, db.ForeignKey('herb.herb_id'))
     # 일반속성
     title = db.Column(db.Text)
@@ -83,10 +86,11 @@ class News(db.Model):
     def __repr__(self):
         return '<News %r>' % (self.news_id)
 
+
 # 4-6. 논문
 class Journal(db.Model):
     # PK, FK
-    journal_id = db.Column(db.Integer, primary_key=True )
+    journal_id = db.Column(db.Integer, primary_key=True)
     herb_id_fk = db.Column(db.Integer, db.ForeignKey('herb.herb_id'))
     # 일반속성
     title = db.Column(db.Text)
