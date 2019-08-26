8fc402c4b3fe07d2b2f3a124ffae8a0bb3fae613
Nashor Style <52341427+nashorstyle@users.noreply.github.com>
2019-08-26 16:25:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,39 @@
-from flask import Flask, render_template, request, session, redirect, url_for
-from flask_dropzone import Dropzone
-from models import db, Herb, Location, Category, SimilarityGroup, News, Journal
-from admin import admin, HerbView, LocationView, CategoryView, SimilarityGroupView, NewsView, JournalView
+import base64
+import hashlib
+import io
 import os
-import base64, hashlib
-import cv2
-import tensorflow as tf
-import ml_utils
 
+import cv2
+from flask import Flask, request, session, render_template
+from flask_dropzone import Dropzone
+from numpy import unicode
+import matplotlib.pyplot as plt
+
+import ml_utils
+from admin import admin, HerbView, LocationView, CategoryView, SimilarityGroupView, NewsView, JournalView
+from models import db, Herb, Location, Category, SimilarityGroup, News, Journal
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-
 app = Flask(__name__)
-# configuration
+dropzone = Dropzone()
 app.config.update(
+    # Admin, SQLAlchemy config:
+    SQLALCHEMY_DATABASE_URI='sqlite:///database/herb_medicine.db',
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    FLASK_ADMIN_SWATCH='cosmo',
+    SECRET_KEY='my_secret_key',
+
     # File Upload Path:
     UPLOADED_PATH=os.path.join(basedir, 'static', 'uploads'),
+
+    # Flask-Dropzone config:
     DROPZONE_ALLOWED_FILE_TYPE='image',
     DROPZONE_MAX_FILE_SIZE=10,
     DROPZONE_MAX_FILES=1,
-
-    # admin, sqlalchemy config:
-    SQLALCHEMY_DATABASE_URI='sqlite:///database/herb_medicine.db',
-    FLASK_ADMIN_SWATCH='cosmo',  # admin 테마 지정
-    SECRET_KEY='my_secret_key',  # admin권한을 위한 키생성
-
-    # debug 모드 지정
-    DEBUG=True
 )
-
-db.init_app(app)
-admin.init_app(app)
-
-dropzone = Dropzone()
-# initialization
 dropzone.init_app(app)
+db.init_app(app)
+model = None
 
 # admin custom Modelview
 table_list = [Herb, Location, Category, SimilarityGroup, News, Journal]
@@ -43,47 +41,128 @@ view_list = [HerbView, LocationView, CategoryView, SimilarityGroupView, NewsView
 for table, view in zip(table_list, view_list):
     admin.add_view(view(table, db.session))
 
-model = None
+
+def _create_identifier():
+    base = unicode("%s|%s".format((request.remote_addr,
+                                   request.headers.get("User-Agent")), 'utf8', errors='replace'))
+    hsh = hashlib.md5()
+    hsh.update(base.encode("utf8"))
+    return hsh.hexdigest()
+
+
 @app.route('/load_model/')
 def load_model():
     model_name = './models/maybe_best_model.h5'
     global model
     if model is None:
-        model = tf.keras.models.load_model(model_name)
+        model = ml_utils.load_model(model_name)
     return 'Model loaded'
 
 
-# # models.py -> config에 설정된 db를 생성
-# @app.route('/create_all')
-# def create_all():
-#     db.create_all()
-#     return "create OK"
-@app.route('/predict/')
-def predict():
-    image = ml_utils.get_image(app.config['UPLOADED_PATH'])
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/index/', methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        image = request.files.get('file')
+        filename, ext = ''.join(image.filename.split('.')[:-1]), image.filename.split('.')[-1]
+        filename = hashlib.md5(filename.encode('utf8')).hexdigest()
+        ext = '.' + ext
+
+        directory = os.path.join(ml_utils.PREPARE_PATH, session['id'])
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+
+        directory = os.path.join(directory, 'image')
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+
+        path = os.path.join(directory, filename + ext)
+        image.save(path)
+
+        prepareImage = ml_utils.prepare_image(path)
+
+        beforePath = os.path.join(ml_utils.BEFORE_PATH, session['id'])
+        if not os.path.exists(beforePath):
+            os.mkdir(beforePath)
+
+        beforePath = os.path.join(beforePath, 'image')
+        if not os.path.exists(beforePath):
+            os.mkdir(beforePath)
+
+        beforePath = os.path.join(beforePath, filename + ext)
+        cv2.imwrite(beforePath, prepareImage)
+
+        session['image'] = image.read()
+        # session['image_path'] = directory
+        print(session['image'])
+    session['id'] = _create_identifier()
+    return render_template('index.html')
+
+
+@app.route('/grad_cam/')
+def grad_cam():
+    global model
+    image = ml_utils.get_image(session['id'])
+    gradCamGrid = ml_utils.grad_cam(model, image)
+
+    img = io.BytesIO()
+    plt.imshow(gradCamGrid)
+    plt.savefig(img, format='png')
+    img.seek(0)
+    img = base64.b64encode(img.getvalue()).decode()
+    plt.close()
+    return f'data:image/png;base64,{img}'
+
+
+@app.route('/smooth_grad/')
+def smooth_grad():
+    global model
+    image = ml_utils.get_image(session['id'])
+    smoothGradGrid = ml_utils.smooth_grad(model, image)
+
+    img = io.BytesIO()
+    plt.imshow(smoothGradGrid)
+    plt.savefig(img, format='png')
+    img.seek(0)
+    img = base64.b64encode(img.getvalue()).decode()
+    plt.close()
+    return f'data:image/png;base64,{img}'
+
+
+@app.route('/lime/')
+def lime():
+    global model
+    image = ml_utils.get_image(session['id'])
+    _, _, _, features, _ = ml_utils.lime(model, image)
+
+    img = io.BytesIO()
+    plt.imshow(features)
+    plt.savefig(img, format='png')
+    img.seek(0)
+    img = base64.b64encode(img.getvalue()).decode()
+    plt.close()
+    return f'data:image/png;base64,{img}'
+
+
+@app.route('/result/')
+def result():
+    image = ml_utils.get_image(session['id'])
     # gradCamGrid = ml_utils.grad_cam(model, image)
     # smoothGradGrid = ml_utils.smooth_grad(model, image)
     # preds, temp, mask, features, out = ml_utils.lime(model, image)
 
     # data = {
     #     'origin_img': session['image'],
-    #     'preds': preds,
-    #     'temp': temp,
-    #     'mask': mask,
-    #     'features': features,
-    #     # 'gradCam': gradCamGrid,
-    #     # 'smoothGradGrid': smoothGradGrid,
-    #     'out': out
+    # 'preds': preds,
+    # 'temp': temp,
+    # 'mask': mask,
+    # 'features': features,
+    # 'gradCam': gradCamGrid,
+    # 'smoothGradGrid': smoothGradGrid,
+    # 'out': out
     # }
 
-    return render_template('predict.html', **data)
-
-@app.route('/result/')
-@app.route('/result/<herb_id>')
-def result(herb_id=2):
     result_id = 2
-    if herb_id:
-        result_id=herb_id
 
     herb = Herb.query.filter_by(herb_id=result_id).first()
     category = Category.query.filter_by(category_id=herb.category_id_fk).first()
@@ -94,12 +173,12 @@ def result(herb_id=2):
     # 1. 해당 유사약재그룹id를 가진 약재 모두다 조회
     group_herbs = Herb.query.filter_by(group_id_fk=group.group_id).all()
     # 2. 유사약재허브들 객체들에서 이름, 이미지경로 가져오기
-    groups = [ (_.herb_name, _.image_path) for _ in group_herbs]
+    groups = [(_.herb_name, _.image_path) for _ in group_herbs]
 
     location_list = Location.query.filter_by(herb_id_fk=result_id).all()
     # 좌표들의 평균구하기
     x_avg, y_avg = 0., 0.
-    for x, y in [ (location.pos_x, location.pos_y) for location in location_list]:
+    for x, y in [(location.pos_x, location.pos_y) for location in location_list]:
         x_avg += x
         y_avg += y
     n = len(location_list)
@@ -108,49 +187,19 @@ def result(herb_id=2):
 
     news_list = News.query.filter_by(herb_id_fk=result_id).all()[:3]
 
-
     data = {
         'herb': herb,
-        'category' : category,
-        'group_name' : group_name,
-        'groups' : groups, # 튜플로 유사그룹 name, img_path가 담김
-        'location_list' : location_list,
-        'location_avg' : (x_avg, y_avg), # 백단에서 계산된 x, y좌표들의 평균
-        'news_list' : news_list,
-        'img_path' : session['image']
-
+        'category': category,
+        'group_name': group_name,
+        'groups': groups,  # 튜플로 유사그룹 name, img_path가 담김
+        'location_list': location_list,
+        'location_avg': (x_avg, y_avg),  # 백단에서 계산된 x, y좌표들의 평균
+        'news_list': news_list,
+        'img_path': session['image']
     }
-    # 'img_path': session['image']
+
     return render_template('app.html', **data)
 
 
-@app.route('/')
-@app.route('/index/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/', methods=['POST', 'GET'])
-def upload():
-    if request.method == 'POST':
-        image = request.files.get('file')
-        filename, ext = ''.join(image.filename.split('.')[:-1]), image.filename.split('.')[-1]
-        filename = hashlib.md5(filename.encode('utf8')).hexdigest()
-        ext = '.' + ext
-
-        print(filename+ext)
-        path = os.path.join(ml_utils.PREPARE_PATH, filename+ext)
-        image.save(path)
-
-        prepareImage = ml_utils.prepare_image(path)
-        beforePath = os.path.join(ml_utils.BEFORE_PATH, filename+ext)
-        cv2.imwrite(beforePath, prepareImage)
-
-        session['image'] =image.read()
-
-        return redirect(url_for('result'))
-    return render_template('index.html')
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
