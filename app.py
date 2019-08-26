c4e4efa273f70a37a871c420af53145f3d737ecb
sunde41 <sunde41@gmail.com>
2019-08-26 22:49:06 +0900

    sun

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ app.config.update(
     DROPZONE_MAX_FILE_SIZE=10000,
     DROPZONE_MAX_FILES=1,
     DROPZONE_IN_FORM=True,
-    DROPZONE_UPLOAD_ON_CLICK=True,
+    DROPZONE_UPLOAD_ON_CLICK=False,
     DROPZONE_UPLOAD_ACTION='result',  # URL or endpoint
     DROPZONE_UPLOAD_BTN_ID='submit',
 )
