3200f3e1e84380521dceb5fbda2c2e119cc6a24a
sunde41 <sunde41@gmail.com>
2019-08-26 22:44:33 +0900

    sun

--- gunicorn.py
+++ gunicorn.py
@@ -6,7 +6,7 @@ import multiprocessing
 bind = "127.0.0.1:5000"
 # workers = multiprocessing.cpu_count() * 2 + 1   # workers=1
 workers=1
-worker_class = "eventlet"   #pip install gunicorn[eventlet] 설치
+# worker_class = "eventlet"   #pip install gunicorn[eventlet] 설치
 reload = True
 loglevel = "error"
 preload_app = True
